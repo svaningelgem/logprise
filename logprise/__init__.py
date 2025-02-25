4d47b9b5e01dd113c69bc44c3063bae266cd64a3
Steven Van Ingelgem <steven@vaningelgem.be>
2025-02-25 18:47:37 +0100

    feat: don't clear logs when sending failed + send them in text format (#6)
    * Don't send the buffer when notifying failed
    
    * Change body format to TEXT
    
    * Ignore extra stuff

--- logprise/__init__.py
+++ logprise/__init__.py
@@ -10,6 +10,7 @@ from typing import TYPE_CHECKING, ClassVar
 
 import apprise.cli
 import loguru._logger
+from apprise.common import NotifyFormat
 from loguru import logger
 
 
@@ -170,9 +171,9 @@ class Appriser:
             return
 
         # Format the buffered logs into a single message
-        message = "".join(self.buffer)
-        self.apprise_obj.notify(title="Script Notifications", body=message)
-        self.buffer.clear()  # Clear the buffer after sending
+        message = "".join(self.buffer).replace("\r", "")
+        if self.apprise_obj.notify(title="Script Notifications", body=message, body_format=NotifyFormat.TEXT):
+            self.buffer.clear()  # Clear the buffer after sending
 
 
 appriser = Appriser()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,25 @@
+import contextlib
+
 import pytest
 from apprise import Apprise
 
+import logprise
 
-@pytest.fixture
+
+@pytest.fixture(autouse=True)
 def notify_mock(monkeypatch):
     """Only mock the notify method of Apprise"""
     calls = []
 
-    def mock_notify(self, title, body):
+    def mock_notify(self, title, body, *_, **__):
         calls.append({"title": title, "body": body})
         return True
 
     monkeypatch.setattr(Apprise, "notify", mock_notify)
     return calls
+
+
+@pytest.fixture(autouse=True)
+def silence_logger():
+    with contextlib.suppress(ValueError):
+        logprise.logger.remove(0)
