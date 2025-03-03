f1628628d82730ddcb7a04403bc75c4a8a2e7dcf
Steven Van Ingelgem <steven@vaningelgem.be>
2025-03-03 07:41:49 +0100

    Also check the case where we remove only 1 particular handler

--- tests/test_prevent_removal.py
+++ tests/test_prevent_removal.py
@@ -1,3 +1,5 @@
+import sys
+
 import loguru
 import loguru._simple_sinks
 
@@ -23,3 +25,24 @@ def test_prevent_removal_of_accumulator():
     new_sink_function = core.handlers[current_id + 1]._sink._function
     assert old_sink_function is not new_sink_function
     assert "bound method Appriser.accumulate_log" in str(new_sink_function)
+
+
+def test_prevent_removal_of_accumulator_not_removing_it():
+    """Test that Appriser doesn't meddle in other handlers"""
+    core = logger._core
+
+    assert len(core.handlers) == 1  # 1 because we have a silence-autofixture
+    accumulator_id: int = next(iter(core.handlers))
+
+    mock_id = logger.add(sys.stderr)
+    assert len(core.handlers) == 2
+
+    assert type(core.handlers[accumulator_id]._sink) is loguru._simple_sinks.CallableSink
+
+    old_sink_function = core.handlers[accumulator_id]._sink._function
+    assert "bound method Appriser.accumulate_log" in str(old_sink_function)
+
+    logger.remove(mock_id)  # Only remove the mocked handler
+
+    assert len(core.handlers) == 1
+    assert accumulator_id in core.handlers  # Nothing should have changed!
