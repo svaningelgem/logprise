e546e07e8527380bfa25b5c61a27ad0d53af7c09
Steven Van Ingelgem <steven@vaningelgem.be>
2025-04-25 17:27:27 +0200

    Reformat

--- tests/test_stupid_tests.py
+++ tests/test_stupid_tests.py
@@ -9,10 +9,12 @@ def test_ignore_intellij_in_stacktrace(mocker: pytest_mock.MockerFixture) -> Non
     fake_frame = mocker.Mock(f_code=mocker.Mock(co_filename="some/jetbrains/intellij/file"))
     assert sut._should_ignore_this_frame(fake_frame) is True
 
+
 def test_ignore_string_in_stacktrace(mocker: pytest_mock.MockerFixture) -> None:
     sut = InterceptHandler()
     fake_frame = mocker.Mock(f_code=mocker.Mock(co_filename="<string>"))
     assert sut._should_ignore_this_frame(fake_frame) is True
+
 
 def test_ignore_current_logprise_file_in_stacktrace(mocker: pytest_mock.MockerFixture) -> None:
     sut = InterceptHandler()
